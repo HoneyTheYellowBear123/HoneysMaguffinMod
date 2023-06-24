d6fbd48864dd49ce82f2ed657d03435924d778d1
Honey the Bear <53951776+HoneyTheYellowBear123@users.noreply.github.com>
2023-06-24 19:43:32 -0400

    initial commit

---
+++ HoneysMaguffinMod/Core/Honey_Macguffin_Base.sql
@@ -0,0 +1,8 @@
+-- MacguffinBase
+-- Author: HoneyTheBear
+-- DateCreated: 6/24/2023 7:38:23 PM
+--------------------------------------------------------------
+
+--Base file for the kinds and such that I don't want to deal with elsewhere
+
+INSERT INTOv 
