8c1eb59b5fbc5f7ad9ab5639794f86e75e0f3e9f
Honey the Bear <53951776+HoneyTheYellowBear123@users.noreply.github.com>
2023-08-06 20:53:30 -0400

    melee buff workin

--- HoneysMaguffinMod/lua/HoneyMacguffin.lua
+++ HoneysMaguffinMod/lua/HoneyMacguffin.lua
@@ -7,7 +7,7 @@
 
 --give player0 (human) a great person for debugging
 local DebugGreatPersonClass = GameInfo.GreatPersonClasses["GREAT_PERSON_HONEY_MACGUFFIN_GP"].Index;
-local DebugGreatPerson = GameInfo.GreatPersonIndividuals["GREAT_PERSON_HONEY_MACGUFFIN_PASSIVE_FLAT_PRODUCTION_GP"].Index;
+local DebugGreatPerson = GameInfo.GreatPersonIndividuals["GREAT_PERSON_HONEY_MACGUFFIN_PASSIVE_MELEE_BUFF_GP"].Index;
 local DebugGreatPerson2 = GameInfo.GreatPersonIndividuals["GREAT_PERSON_INDIVIDUAL_BHASA"].Index;
 local altarBuildingIndex = GameInfo.Buildings["BUILDING_HONEY_MACGUFFIN_HOLDER_EMPTY"].Index
 
