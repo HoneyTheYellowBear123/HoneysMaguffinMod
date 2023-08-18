12db966755c82cff7aca547c72a094cda8da4fa8
Honey the Bear <53951776+HoneyTheYellowBear123@users.noreply.github.com>
2023-08-18 17:50:23 -0400

    doom hoof

--- HoneysMaguffinMod/lua/HoneyMacguffin.lua
+++ HoneysMaguffinMod/lua/HoneyMacguffin.lua
@@ -7,7 +7,7 @@
 
 --give player0 (human) a great person for debugging
 local DebugGreatPersonClass = GameInfo.GreatPersonClasses["GREAT_PERSON_HONEY_MACGUFFIN_GP"].Index;
-local DebugGreatPerson = GameInfo.GreatPersonIndividuals["GREAT_PERSON_HONEY_MACGUFFIN_PASSIVE_LIGHT_CAVALRY_BUFF_GP"].Index;
+local DebugGreatPerson = GameInfo.GreatPersonIndividuals["GREAT_PERSON_HONEY_MACGUFFIN_PASSIVE_HEAVY_CAVALRY_BUFF_GP"].Index;
 local DebugGreatPerson2 = GameInfo.GreatPersonIndividuals["GREAT_PERSON_INDIVIDUAL_BHASA"].Index;
 local altarBuildingIndex = GameInfo.Buildings["BUILDING_HONEY_MACGUFFIN_HOLDER_EMPTY"].Index
 
